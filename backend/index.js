6886a53723fe03da2c97b71b63ec1902854f3493
Markz666 <mzhao757@gmail.com>
2018-04-07 14:21:01 -0400

    add some stuff

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,8 @@
 const express = require('express');
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const bluebird = require("bluebird");
+const fs = require('fs');
+const im = require('imagemagick');
+// var base64Img = require('base64-img');
