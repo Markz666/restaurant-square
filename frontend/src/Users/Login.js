c0975d222654b79ca1a72cd4d676ef6c9ebff5aa
Markz666 <mzhao757@gmail.com>
2018-04-25 16:23:50 -0400

    add socket.io-client

--- frontend/src/Users/Login.js
+++ frontend/src/Users/Login.js
@@ -4,6 +4,8 @@ import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
+import io from 'socket.io-client';
+const socket = io('http://localhost:3001');
 
 let muiTheme = getMuiTheme({
     fontFamily: 'Microsoft YaHei'
