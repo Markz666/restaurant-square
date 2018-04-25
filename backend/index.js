61b7971b86ba9804107096fac6a5c80ddb770ab3
Markz666 <mzhao757@gmail.com>
2018-04-25 18:29:21 -0400

    signup function

--- backend/index.js
+++ backend/index.js
@@ -5,13 +5,14 @@ const io = require('socket.io')(http);
 const bluebird = require("bluebird");
 const fs = require('fs');
 const im = require('imagemagick');
+const bcrypt = require("bcrypt");
 // const base64Img = require('base64-img');
 const usersAPI = require("./db/users.js");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(function(req,res,next){
+app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
@@ -40,6 +41,20 @@ io.on('connection', socket => {
 
     socket.on("signup", async(userInfo) => {
         console.log(userInfo);
+        try {
+            const saltRounds = 4;
+            let hashed_password = await bcrypt.hash(userInfo.password1, saltRounds);
+            await usersAPI.addUser(userInfo.userName, hashed_password, userInfo.email, userInfo.phone);
+            console.log("Create user success!");
+            // res.render('body/private');
+            let status = "success";
+            socket.emit("loggedIn", status);
+        } catch (e) {
+            const user = await usersAPI.getUserByUsername(userInfo.userName);
+            if (user) {
+                res.redirect('/signup_err');
+            }
+        }
     });
     
     socket.on('disconnect', () => {
