90c8bfad2bb4a7bf32937e7fb03bad548707ccac
Tao Deng <635456344@qq.com>
2018-04-28 16:35:40 -0400

    delete top header of restaurant panel

--- frontend/src/Display/RestaurantPanel.js
+++ frontend/src/Display/RestaurantPanel.js
@@ -4,49 +4,6 @@ import favImg from '../img/fav.png'
 import goodImg from '../img/good.png'
 import badImg from '../img/bad.png'
 
-class SearchField extends Component{
-    handleClick() {
-        /*
-        var key_word = document.getElementById('key_word').value;
-        var url = "v1/search?q=" + key_word + "&type=track";
-        sendRequest(client_id, client_secret, url, function(data){
-            updateItems(data);
-        });
-        */
-        return false;
-    }
-
-    render(){
-        return (
-            <div>  
-                <form id="search-form">
-                  <search_title>key word: </search_title>
-                  <input id="key_word"></input>
-                  <input type="button" id="search" value="search" onClick={this.handleClick}></input>
-                </form>
-            </div>  
-        );
-    }
-}
-
-
-class SignInBtn extends Component {
-	handleClick() {
-    	var modal = document.getElementById('sign-in-modal');
-    	modal.style.display = "block";
-
-    	return false;
-  	}
-
-  	render() {  
-        return (  
-            <div>  
-                <input id="signIn" type="button" value="sign in"  onClick={this.handleClick}></input> 
-            </div>  
-        );  
-    }  
-}
-
 class Container extends Component{
     handleClick(){
 
@@ -55,12 +12,6 @@ class Container extends Component{
     render() {
         return (
             <div class="container">
-                <header>
-                  <p>
-                    <span>Restaurant</span>
-                    <br />Plat
-                  </p>
-                </header>
                 <div>
                     <div class="restaurant_img_container">
                         <img id="restaurant_img" alt=""></img>
@@ -145,17 +96,6 @@ class RestaurantPanel extends Component {
   render() {
     return (
       <div className="RestaurantPanel">
-        <header class="top-header">
-		    <nav class="top-nav">
-		      <a href="/">Home</a>
-		      <a href="/about">About</a>
-		    </nav>
-            
-		    <SearchField />
-            <space id="space">heiheiheiheiasdasdasdasdasdasdad</space>
-		    <SignInBtn />
-            <space>123123</space>
-		</header>
         <Container />
       </div>
     );
