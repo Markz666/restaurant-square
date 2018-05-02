9349d01cf173c27062f7c0b548f6d21c76994fcb
Markz666 <mzhao757@gmail.com>
2018-05-02 16:17:48 -0400

    add background color

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import "./App.css";
+import {Helmet} from 'react-helmet';
 import {
   BrowserRouter as Router,
   Route,
@@ -43,27 +44,34 @@ class App extends Component {
   render() {
     return (
       <div>
-      <Header />
-      <Router>
-        <div className="App">
-        {/* <p className="App-intro">{this.state.response}</p> */}
-          <div className="App-body">
-            <Switch>
-              <Route exact path="/" component={Home}/>
-              <Route path="/signup" component={Signup}/>
-              <Route path="/login" component={Login}/>
-              <Route path="/logout" component={Logout}/>
-              <Route path="/login_err" component={Login_Error}/>
-              <Route path="/display" component={RestaurantPanel}/>
-              <Route path="/about" component={About}/>
-              <Route path="*" component={NotFound} />
-              
-              
-            </Switch>
+        <Header />
+        <Router>
+          <div className="App">
+          {/* <p className="App-intro">{this.state.response}</p> */}
+            <div className="App-body">
+              <Switch>
+                <Route exact path="/" component={Home}/>
+                <Route path="/signup" component={Signup}/>
+                <Route path="/login" component={Login}/>
+                <Route path="/logout" component={Logout}/>
+                <Route path="/login_err" component={Login_Error}/>
+                <Route path="/display" component={RestaurantPanel}/>
+                <Route path="/about" component={About}/>
+                <Route path="*" component={NotFound} />
+                        
+              </Switch>
+            </div>
           </div>
+        </Router>
+
+        <div className="body">
+          <Helmet>
+              <style>{'body { background-color: #E3DFDB; }'}</style>
+          </Helmet>
         </div>
-      </Router>
+
       </div>
+  
     );
   }
 }
