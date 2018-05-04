b70e2efa3e2ba40692c56679a61a1911f203b5bd
Tao Deng <635456344@qq.com>
2018-05-04 18:09:44 -0400

    modify the restaurant list page

--- frontend/src/Restaurant/RestaurantList.js
+++ frontend/src/Restaurant/RestaurantList.js
@@ -13,17 +13,13 @@ class RestaurantList extends Component {
                         <Link key={restaurant.id} to={`/restaurant/${restaurant.id}`}>
                           {restaurant.name}
                         </Link>
-                        <h5><b>Artist: </b> 
-                        <Link key={restaurant.album.artists[0].id} to={`/artists/${restaurant.album.artists[0].id}`}>
-                          {restaurant.album.artists[0].name}
-                        </Link></h5>
+                        <h5><b>categories: </b></h5>
             
-                        <h5>popularity: {restaurant.popularity}</h5>
-                        <h5><b>Album: </b>
-                        <Link key={restaurant.album.id} to={`/albums/${restaurant.album.id}`}>
-                         {restaurant.album.name}
-                        </Link></h5>
-                        <img className="img" src={restaurant.album.images[0].url} alt={restaurant.name}/>;
+                        <h5>rating: {restaurant.rating}</h5>
+                        <h5>review count: {restaurant.review_count}</h5>
+                        <h5>status: {restaurant.is_closed ? 'closed' : 'open'}</h5>
+                        <h5>location: {restaurant.location.address1 + ", " + restaurant.location.city + ", " + restaurant.location.state + ", " + restaurant.location.country + ", " + restaurant.location.zip_code}</h5>
+                        <img className="restaurant_list_img" src={restaurant.image_url} alt={restaurant.name}/>;
                       </ul>
                     </div>
                 );
